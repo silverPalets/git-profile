d12f273f9161df0a939de6b248a9d71fcd07ddf9
silverPalets <hamidabbasi1382@gmail.com>
2025-02-18 17:38:43 +0330

    adding search feature, cleaner html, fixing ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-const userName = "amirrezahekmati";
+const username = "amirrezahekmati";
 
-async function getProfileData(userName) {
-  let promise = await fetch(`https://api.github.com/users/${userName}`)
+renderInfo();
+
+async function getProfileData(username) {
+  console.log(username);
+  let promise = await fetch(`https://api.github.com/users/${username}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(`status: ${response.status}`);
@@ -17,3 +20,48 @@ async function getProfileData(userName) {
 
   return promise;
 }
+
+function createInfoText(info) {
+  let text = `
+  bio: ${info.bio} <br>
+  blog: ${info.blog} <br>
+  company: ${info.company} <br>
+  email: ${info.email} <br>
+  followers number: ${info.followers} <br>
+  following number: ${info.following} <br>
+  repository: ${info.repos_url} <br>
+  profile url: <a href="${info.html_url}">here</a> <br>
+  `;
+
+  return text;
+}
+
+function updateUserInfo(information) {
+  document.querySelector('.js-user-info')
+  .innerHTML = information;
+}
+
+function updateUserImg(imgUrl) {
+  let image = document.querySelector('.js-user-img');
+  image.src = imgUrl;
+}
+
+function renderInfo() {
+
+  let usernameForm = document.querySelector('.js-username-form');
+  usernameForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    let username = usernameForm.querySelector('.js-user-input').value;
+
+    let info = await getProfileData(username);
+
+    updateUserImg(info.avatar_url);
+
+    let information = createInfoText(info);
+    updateUserInfo(information);
+  });
+}
+
+// getProfileData('amirrezahekmati').then( info => {
+//   console.log(info);
+// });
